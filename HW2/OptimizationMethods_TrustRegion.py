99b1eac81141648dc57dea7386cc96b17d79aaef
rezajebeli97 <rezajebeli97@yahoo.com>
2019-05-19 12:39:00 +0430

    start of trust region methods

--- HW2/OptimizationMethods_TrustRegion.py
+++ HW2/OptimizationMethods_TrustRegion.py
@@ -29,15 +29,10 @@ def hessian(x):
     return g
 
 def estimate_hessian(x, gradient, b, y, s):
-    tmp1 = np.matmul(np.matmul(np.matmul(b,s), s.T), b)
-    [[tmp1_makhraj]] = np.matmul(np.matmul(s.T, b), s)
-    tmp1 = tmp1 / tmp1_makhraj
-
-    tmp2 = np.matmul(y, y.T)
-    [[tmp2_makhraj]] = np.matmul(y.T , s)
-    tmp2 = tmp2 / tmp2_makhraj
-
-    return b - tmp1 + tmp2
+    print(np.shape(b))
+    print(np.shape(y))
+    print(np.shape(s))
+    return hessian(x)
 
 
 def initialState():
@@ -101,8 +96,8 @@ def quasi_newton_BFGS(maxIteration, maxAlpha):
     b = hessian(x)
     while f_x > 0.00000000000000000000000001 and iteration < maxIteration:
         g = gradient(x)
-        if iteration != 0:
-            y = np.array(g) - np.array(gradient(prevx))
+        if prevx != None:
+            y = g - gradient(prevx)
             s = x - prevx
             b = estimate_hessian(x, g, b, y, s)
 
@@ -115,6 +110,50 @@ def quasi_newton_BFGS(maxIteration, maxAlpha):
     print("x = ", x)
     print("f(x) = ", f_x)
     return x
+
+
+def cauchy_point(delta0, efficent, maxIteration):        #delta0 bigger than 0 , first delta should initialize sth between 0 and delta0, efficient should be sth between 0 and 0.25
+
+    delta = delta0/2
+    x = initialState()
+    f_x = f(x)
+    iteration = 0
+    while f_x > 0.00000000000000000000000001 and iteration < maxIteration:
+        g = gradient(x)
+        g = np.array(g)
+        b = hessian(x)
+        b = np.array(b)
+        ps = ((-1 * delta) / np.linalg.norm(g)) * g
+        [[condition]] = np.matmul(np.matmul(g.T, b), g)
+        tou = 0
+        if condition <= 0:
+            tou = 1
+        else:
+            tou = min(1, np.linalg.norm(g) ** 3 / (delta * condition))
+        pc = (-1 * tou) * ps            #cauchy point
+        m_0 = f(x)
+        m_pc = f(x + pc) + np.matmul(pc.T, g) + 0.5 * np.matmul(np.matmul(pc.T, b), pc) #[[]]
+        efficency = (f(x) - f(x + pc)) / (m_0 - m_pc)
+
+        if efficency < 0.25:
+            delta = 0.25* delta
+        else:
+            if efficency > 3/4 and np.linalg.norm(pc) == delta:
+                delta = min(2*delta, delta0)
+            else:
+                delta = delta
+
+        if efficency > efficent:
+            x = x + pc
+        else:
+            x = x
+
+        f_x = f(x)
+        iteration += 1
+    print("x = ", x)
+    print("f(x) = ", f_x)
+    return x
+
 
 def compare(beta):
     a = np.load('data.npz')
@@ -136,6 +175,6 @@ def compare(beta):
     plotter.show()
 
 
-beta = quasi_newton_BFGS(10000, 1)
+beta = cauchy_point(1, 0.15, 10000)
 # beta = stochastic(5000, 0.01)
 # compare(beta=beta)
